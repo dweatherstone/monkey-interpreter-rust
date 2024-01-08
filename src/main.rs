885c1bb047d076c4872f7cd2411fea79c194bfb5
David Weatherstone <davidweatherstone@gmail.com>
2024-01-08 17:06:42 +0000

    Basic REPL created for calling the Lexer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #![allow(unused)]
 
+use std::io;
+
+use crate::repl::start;
+
 pub mod lexer;
+pub mod repl;
 pub mod token;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello! This is the basic interpreter");
+    println!("Write the code below:");
+    start(io::stdin(), io::stdout());
 }
