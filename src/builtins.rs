8a4eb27c0ddafede3551f7ec3890571c79cfece8
David Weatherstone <davidweatherstone@gmail.com>
2024-01-22 15:01:38 +0000

    Adding puts build-in function

--- src/builtins.rs
+++ src/builtins.rs
@@ -10,6 +10,7 @@ impl Builtins {
             (String::from("last"), Object::Builtin(b_last)),
             (String::from("rest"), Object::Builtin(b_rest)),
             (String::from("push"), Object::Builtin(b_push)),
+            (String::from("puts"), Object::Builtin(b_puts)),
         ]
     }
 }
@@ -117,4 +118,11 @@ fn b_push(args: Vec<Object>) -> Object {
             other.object_type()
         )),
     }
+}
+
+fn b_puts(args: Vec<Object>) -> Object {
+    for arg in args {
+        println!("{}", arg);
+    }
+    NULL
 }
